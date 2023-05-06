02b40ec8a33dddaf8a70b3a6b9ab71bb71b7f364
Nuria Antón Calle <nurianton1999@gmail.com>
2023-05-06 14:12:25 +0200

    Agregar archivos de proyecto.

---
+++ SistemaVenta AplicacionWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaVenta_AplicacionWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
