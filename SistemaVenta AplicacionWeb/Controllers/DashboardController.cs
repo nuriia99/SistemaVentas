aafa51cb75be1cf99b193fd9f0a18bb95a53dbe7
Nuria Antón Calle <nurianton1999@gmail.com>
2023-05-07 13:17:48 +0200

    cupy html template and views

---
+++ SistemaVenta AplicacionWeb/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta_AplicacionWeb.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.BLL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+
+        public static void InjectarDependencia(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+    }
+}
